850dbf25441ac0dfca165a22f6f8ff3efebf167d
Nik Bel <bf109e1111@gmail.com>
2025-03-09 21:48:10 +0300

    feat: add page (home, addUser), add table prototype

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/users", (req,res)=>{
-  res.send("All users");
+router.get("/", (req,res)=>{
+  res.render("index", {title: "Home Page"});
+});
+
+router.get("/add", (req,res)=>{
+  res.render("add_users", {title: "Add Users"});
 });
 
 module.exports = router;
